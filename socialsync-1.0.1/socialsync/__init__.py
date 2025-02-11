cac9f792e344069aed2b535d8d5a95b7ce04eec6
Mark Lubrance <markjohnson79@myyahoo.com>
2025-02-11 05:38:41 -0500

    Delete socialsync-1.0.1 directory

--- socialsync-1.0.1/socialsync/__init__.py
+++
@@ -1,7 +0,0 @@
-from .api import SocialSyncAPI
-from .auth import AuthHandler
-from .utils import validate_response
-from .exceptions import SocialSyncError
-from .analytics import TrendAnalyzer
-
-__all__ = ["SocialSyncAPI", "AuthHandler", "validate_response", "SocialSyncError", "TrendAnalyzer"]
